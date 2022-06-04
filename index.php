470126b4661a241420145b94f48506d78cbb5ccb
rustamnew <1rustamnew1@gmail.com>
2022-06-04 16:03:54 +0300

    +YooKassa

--- index.php
+++ index.php
@@ -91,7 +91,73 @@ $APPLICATION->SetTitle("Главная");
 	),
 	false
 );?>
+
 <?$APPLICATION->IncludeComponent(
+	"codekeepers:news.detail", 
+	"gallery-detail", 
+	array(
+		"COMPONENT_TEMPLATE" => "gallery-detail",
+		"IBLOCK_TYPE" => "content",
+		"IBLOCK_ID" => "30",
+		"ELEMENT_ID" => "149",
+		"ELEMENT_CODE" => "",
+		"CHECK_DATES" => "Y",
+		"FIELD_CODE" => array(
+			0 => "",
+			1 => "",
+		),
+		"PROPERTY_CODE" => array(
+			0 => "title",
+			1 => "name",
+			2 => "subtitle",
+			3 => "images",
+			4 => "",
+		),
+		"IBLOCK_URL" => "",
+		"DETAIL_URL" => "",
+		"AJAX_MODE" => "N",
+		"AJAX_OPTION_JUMP" => "N",
+		"AJAX_OPTION_STYLE" => "Y",
+		"AJAX_OPTION_HISTORY" => "N",
+		"AJAX_OPTION_ADDITIONAL" => "",
+		"CACHE_TYPE" => "A",
+		"CACHE_TIME" => "36000000",
+		"CACHE_GROUPS" => "Y",
+		"SET_TITLE" => "Y",
+		"SET_CANONICAL_URL" => "N",
+		"SET_BROWSER_TITLE" => "Y",
+		"BROWSER_TITLE" => "-",
+		"SET_META_KEYWORDS" => "Y",
+		"META_KEYWORDS" => "-",
+		"SET_META_DESCRIPTION" => "Y",
+		"META_DESCRIPTION" => "-",
+		"SET_LAST_MODIFIED" => "N",
+		"INCLUDE_IBLOCK_INTO_CHAIN" => "Y",
+		"ADD_SECTIONS_CHAIN" => "Y",
+		"ADD_ELEMENT_CHAIN" => "N",
+		"ACTIVE_DATE_FORMAT" => "d.m.Y",
+		"USE_PERMISSIONS" => "N",
+		"STRICT_SECTION_CHECK" => "N",
+		"DISPLAY_DATE" => "Y",
+		"DISPLAY_NAME" => "Y",
+		"DISPLAY_PICTURE" => "Y",
+		"DISPLAY_PREVIEW_TEXT" => "Y",
+		"USE_SHARE" => "N",
+		"PAGER_TEMPLATE" => ".default",
+		"DISPLAY_TOP_PAGER" => "N",
+		"DISPLAY_BOTTOM_PAGER" => "Y",
+		"PAGER_TITLE" => "Страница",
+		"PAGER_SHOW_ALL" => "N",
+		"PAGER_BASE_LINK_ENABLE" => "N",
+		"SET_STATUS_404" => "N",
+		"SHOW_404" => "N",
+		"MESSAGE_404" => "",
+		"slider" => "Y"
+	),
+	false
+);?>
+
+<?/*$APPLICATION->IncludeComponent(
 	"codekeepers:news.list", 
 	"gallery-list", 
 	array(
@@ -159,7 +225,7 @@ $APPLICATION->SetTitle("Главная");
 		"TITLE" => "Фотогалерея"
 	),
 	false
-);?>
+);*/?>
 <?$APPLICATION->IncludeComponent(
 	"codekeepers:news.list",
 	"banner_call-to-action",
